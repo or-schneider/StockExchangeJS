d9a53b851c7b9edb3d10a1d5973fd2a48613fd48
or-schneider <orschneider@protonmail.com>
2021-11-03 18:38:08 +0200

    M6(F): Transforms searchbar functionality into a class

--- search_bar/search_bar.js
+++ search_bar/search_bar.js
@@ -2,84 +2,93 @@ import {fetchAsync} from "../scripts/fetchAsync.js";
 import {fetchCompanyDataAsync} from "../company/companyProfile.js";
 import {update as updatePriceChangesNode} from "../price_changes/price_changes_updater.js"
 
-const inputButtonNode = document.getElementById("searchBarInputButton");
-const inputNode = document.getElementById("searchBarInput");
-const resultsLoaderNode = document.getElementById("searchBarResultsLoader");
-const resultsListNode = document.getElementById("searchBarResultsList");
-const resultNodes = resultsListNode.querySelectorAll(".search-bar-results-list-result");
+class SearchBar{
+    inputButtonNode;
+    inputNode = document;
+    resultsLoaderNode;
+    resultsListNode;
 
-const resultLocalUrl = "./company/company.html";
+    resultLocalUrl = "./company/company.html";
 
-function submitSearch(event){
-    event.preventDefault();
-    search(inputNode.value);   
-}
-async function search(searchString){
-    resultsLoaderNode.classList.remove("d-none");
-    
-    let results = await fetchResults(searchString);
-    
-
-    let companyProfiles = [];
-    for (const result of results) {
-        let companyProfile = await fetchCompanyDataAsync(result.symbol);
-        companyProfiles.push(companyProfile);
+    constructor(){
+        this.init();    
     }
-    updateResults(results,companyProfiles)
+    init(){
+        this.inputButtonNode = document.getElementById("searchBarInputButton");
+        this.inputNode = document.getElementById("searchBarInput");
+        this.resultsLoaderNode = document.getElementById("searchBarResultsLoader");
+        this.resultsListNode = document.getElementById("searchBarResultsList");
+        this.resultNodes = this.resultsListNode.querySelectorAll(".search-bar-results-list-result");
 
-    resultsLoaderNode.classList.add("d-none");
-}
-function hideResults(startIndex){
-    for (let i = startIndex; i < resultNodes.length; i++) {
-        const resultNode = resultNodes[i];
-        resultNode.classList.add("d-none");
+        this.inputButtonNode.addEventListener('click',this.submitSearch.bind(this))
+        this.hideResults(0);
     }
-    if(startIndex==0)
-        resultsListNode.classList.add("d-none");
-}
-function showResults(endIndex){
-    if(endIndex>0)
-        resultsListNode.classList.remove("d-none");
-    for (let i = 0; i < endIndex; i++) {
-        const resultNode = resultNodes[i];
-        resultNode.classList.remove("d-none");
+    submitSearch(event){
+        event.preventDefault();
+        this.search(this.inputNode.value);   
     }
-}
-
-async function fetchResults(searchTarget){
-    const url = `${STOCK_EXCHANGE_API_ROOT_URL}search?query=${searchTarget}&limit=10&exchange=NASDAQ`
-    let data = fetchAsync(url);
-    return data;
-}
-function updateResults(results,companyProfiles){
-    let endIndex = Math.min(results.length,resultNodes.length);
-    showResults(endIndex);
-    for (let i = 0; i < endIndex; i++) {
-        let result = results[i];
-        let resultNode = resultNodes[i];
-
-        let companyNameNode = resultNode.querySelector(".search-bar-results-list-result-company-name");
-        companyNameNode.textContent = result.name;
+    async search(searchString){
+        this.resultsLoaderNode.classList.remove("d-none");
         
-        let symbolNode = resultNode.querySelector(".search-bar-results-list-result-symbol");
-        symbolNode.textContent = `(${result.symbol})`;
+        let results = await this.fetchResults(searchString);
 
-        let linkNode = resultNode.querySelector(".search-bar-results-list-result-link");
-        linkNode.href = `${resultLocalUrl}?symbol=${result.symbol}`
-        let priceChangesNode = resultNode.querySelector(".search-bar-results-list-result-price-changes");
-        updatePriceChangesNode(priceChangesNode,companyProfiles[i].profile.changesPercentage);
-        
-        let imageNode = resultNode.querySelector(".search-bar-results-list-result-image");
-        imageNode.src = companyProfiles[i].profile.image;
-        
+        let companyProfiles = [];
+        for (const result of results) {
+            let companyProfile = await fetchCompanyDataAsync(result.symbol);
+            companyProfiles.push(companyProfile);
+        }
+        this.updateResults(results,companyProfiles)
 
+        this.resultsLoaderNode.classList.add("d-none");
     }
-    hideResults(endIndex);
+    hideResults(startIndex){
+        for (let i = startIndex; i < this.resultNodes.length; i++) {
+            const resultNode = this.resultNodes[i];
+            resultNode.classList.add("d-none");
+        }
+        if(startIndex==0)
+            this.resultsListNode.classList.add("d-none");
+    }
+    showResults(endIndex){
+        if(endIndex>0)
+            this.resultsListNode.classList.remove("d-none");
+        for (let i = 0; i < endIndex; i++) {
+            const resultNode = this.resultNodes[i];
+            resultNode.classList.remove("d-none");
+        }
+    }
+
+    async fetchResults(searchTarget){
+        const url = `${STOCK_EXCHANGE_API_ROOT_URL}search?query=${searchTarget}&limit=10&exchange=NASDAQ`
+        let data = fetchAsync(url);
+        return data;
+    }
+    updateResults(results,companyProfiles){
+        let endIndex = Math.min(results.length,this.resultNodes.length);
+        this.showResults(endIndex);
+        for (let i = 0; i < endIndex; i++) {
+            let result = results[i];
+            let resultNode = this.resultNodes[i];
+
+            let companyNameNode = resultNode.querySelector(".search-bar-results-list-result-company-name");
+            companyNameNode.textContent = result.name;
+            
+            let symbolNode = resultNode.querySelector(".search-bar-results-list-result-symbol");
+            symbolNode.textContent = `(${result.symbol})`;
+
+            let linkNode = resultNode.querySelector(".search-bar-results-list-result-link");
+            linkNode.href = `${this.resultLocalUrl}?symbol=${result.symbol}`
+            let priceChangesNode = resultNode.querySelector(".search-bar-results-list-result-price-changes");
+            updatePriceChangesNode(priceChangesNode,companyProfiles[i].profile.changesPercentage);
+            
+            let imageNode = resultNode.querySelector(".search-bar-results-list-result-image");
+            imageNode.src = companyProfiles[i].profile.image;
+            
+
+        }
+        this.hideResults(endIndex);
+    }
 
 }
 
-function init(){
-    inputButtonNode.addEventListener('click',submitSearch)
-    hideResults(0);
-}
-init();
+new SearchBar();
