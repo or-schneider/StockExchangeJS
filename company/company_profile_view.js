c60673bea048e8dfeb4847a38f02d729ffc6bd1d
or-schneider <orschneider@protonmail.com>
2021-11-06 21:46:56 +0200

    M9(WIP) Starts transforming CompanyProfile to a class

--- company/company_profile_view.js
+++ company/company_profile_view.js
@@ -6,38 +6,55 @@ const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
 
-const contentContainerNode = document.getElementById("companyProfileContentContainer");
-const loaderNode = document.getElementById("companyProfileLoader");
-const imageNode = document.getElementById("companyProfileImage");
-const nameNode = document.getElementById("companyProfileName");
-const linkNode = document.getElementById("companyProfileHeaderLink");
-const stockPriceNode = document.getElementById("companyProfileStockPrice");
-const stockPriceChangesNode = document.getElementById("companyProfileStockPriceChanges");
-const descriptionNode = document.getElementById("companyProfileStockDescription");
+class CompanyProfile{
+    constructor(containerNode,symbol){
+        this.symbol = symbol;
+        this.containerNode = containerNode;
 
+        this.init();
+    }
+    init(){
+        this.contentContainerNode = document.getElementById("companyProfileContentContainer");
+        this.loaderNode = document.getElementById("companyProfileLoader");
+        this.imageNode = document.getElementById("companyProfileImage");
+        this.nameNode = document.getElementById("companyProfileName");
+        this.linkNode = document.getElementById("companyProfileHeaderLink");
+        this.stockPriceNode = document.getElementById("companyProfileStockPrice");
+        this.stockPriceChangesNode = document.getElementById("companyProfileStockPriceChanges");
+        this.descriptionNode = document.getElementById("companyProfileStockDescription");
 
-async function refresh(){
-    loaderNode.classList.remove("d-none");
-    let companyData = await fetchCompanyDataAsync(params.symbol);
-    updateProfileView(companyData.profile);
-    let historicalPriceData = await fetchHistoricalPriceAsync(params.symbol);
+        this.contentContainerNode.classList.add("d-none");
+        this.loaderNode.classList.add("d-none");
+    }
+    async load(){
+        this.loaderNode.classList.remove("d-none");
+        let companyData = await fetchCompanyDataAsync(params.symbol);
+        this.updateProfileView(companyData.profile);
+        
 
-    companyGraph.updateGraph(historicalPriceData);
+        this.contentContainerNode.classList.remove("d-none");
+        this.loaderNode.classList.add("d-none");
 
-    contentContainerNode.classList.remove("d-none");
-    loaderNode.classList.add("d-none");
+    }
+    updateProfileView(profileData){
+        this.imageNode.src = profileData.image;
+        this.nameNode.textContent = profileData.companyName;
+        this.stockPriceNode.textContent = profileData.price;
+        updatePriceChangesNode(this.stockPriceChangesNode, profileData.changesPercentage)
+        this.descriptionNode.textContent = profileData.description;
+        this.linkNode.href = profileData.website;
+    }
+    async addChart(){
+        loaderNode.classList.remove("d-none");
+
+        let historicalPriceData = await fetchHistoricalPriceAsync(params.symbol);
+
+        companyGraph.updateGraph(historicalPriceData);
+
+        this.loaderNode.classList.add("d-none");
+    }
 }
-function updateProfileView(profileData){
-    imageNode.src = profileData.image;
-    nameNode.textContent = profileData.companyName;
-    stockPriceNode.textContent = profileData.price;
-    updatePriceChangesNode(stockPriceChangesNode, profileData.changesPercentage)
-    descriptionNode.textContent = profileData.description;
-    linkNode.href = profileData.website;
-}
-function init(){
-    contentContainerNode.classList.add("d-none");
-    loaderNode.classList.add("d-none");
-    refresh();
-}
-init();
+
+const companyProfile = new CompanyProfile(contentContainerNode , params.symbol);
+await companyProfile.load();
+await companyProfile.addChart();
